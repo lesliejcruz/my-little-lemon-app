012ed8c78cedd9e99d98a8eb25cf962474de5453
lesliejcruz <lesliecruz0@gmail.com>
2024-08-29 18:12:52 -0400

    bookings page

--- my-little-lemon-app/src/components/Header.js
+++ my-little-lemon-app/src/components/Header.js
@@ -1,6 +1,23 @@
+import React from "react";
+import food from "../images/restauranfood.jpg"
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
-    <header />
+    <header>
+        <section>
+            <div>
+                <h2>Little Lemon</h2>
+                <h3>Chicago</h3>
+                <p>We are a family-owned Mediterranean restaurant
+                    , focused on traditional recipes served with a modern twist.</p>
+                <Link to="/Booking"><button aria-label="On Click">Reserve Table</button></Link>
+            </div>
+            <div>
+                <img src={food} alt="LL appetizer"/>
+            </div>
+        </section>
+    </header>
     )
 }
 
